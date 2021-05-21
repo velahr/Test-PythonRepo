1b0f8d768b45bc597d6b11a9442143dccb96f659
velahr <sparrows@email.com>
2021-05-21 05:18:07 -0400

    Removed some code

--- script.py
+++ script.py
@@ -1,18 +1,14 @@
-import math
-import sys
-from os import rename
+
 
 import requests
 
 #print(sys.version) 
-print(sys.executable)
+
 
 #name = input("Your Name? ")
 #print("Hello,", name)
 
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
+
 
 
 r = requests.get("https://coreyms.com")
